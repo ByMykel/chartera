5906561a05c67790c0e7acd32ae6548388a2df4b
Mihail Dragnev <bymykel5@gmail.com>
2022-09-11 22:23:07 +0200

    Remove redirect to fix Github page URL error.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ChartContainer from './Components/Chart/ChartContainer';
 import Error from './Components/Error/Error';
 import './App.css';
-import { Switch, Route, Redirect } from 'react-router';
+import { Switch, Route } from 'react-router';
 
 const App: React.FC = function () {
   return (
     <Switch>
-      <Route path="/" component={ChartContainer} exact />
+      <Route path="/chartera" component={ChartContainer} exact />
       <Route path="/error" component={Error} />
-      <Route path="/" render={() => <Redirect to="/" />} />
     </Switch>
   );
 };
